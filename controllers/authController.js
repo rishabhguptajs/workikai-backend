5b7d19f1bc15b21607a0ae19e23b3fde9b6f4d29
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-10-06 19:03:55 +0530

    Refactor githubAuthCallback function and add User model and authRoutes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,38 +1,46 @@
 import axios from "axios";
 import User from "../models/User.js";
+import querystring from "querystring"; // To parse URL-encoded responses
 
 export const githubAuthCallback = async (req, res) => {
     const { code } = req.query;
 
     if (!code) {
-        return res.status(400).json({ error: "No code provided" })
+        return res.status(400).json({ error: "No code provided" });
     }
 
     try {
-        const response = await axios.post(`https://github.com/login/oauth/access_token`, {
-            params: {
-                client_id: process.env.GITHUB_CLIENT_ID,
-                client_secret: process.env.GITHUB_CLIENT_SECRET,
-                code: code
-            },
-            headers: {
-                accept: "application/json"
-            }
-        })
+        console.log({
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code: code
+        });
 
-        const { access_token } = response.data;
+        const response = await axios.post(
+            `https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`,
+            {
+                headers: {
+                    accept: "application/json"
+                }
+            }
+        );
+
+        const parsedData = querystring.parse(response.data);
+
+        const { access_token } = parsedData;
 
         if (!access_token) {
-            return res.status(400).json({ error: "Failed to get access token" })
+            return res.status(400).json({ error: "Failed to get access token" });
         }
 
-        const user = new User({ access_token });
-
+        const user = new User({ 
+            githubAccessToken: access_token,
+         });
         await user.save();
 
-        return res.status(200).json({ token: access_token, user })
+        return res.status(200).json({ token: access_token, user });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ error: "Server error", message: error.message })
+        console.error(error);
+        return res.status(500).json({ error: "Server error", message: error });
     }
-}
+};
